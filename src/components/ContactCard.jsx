62d49627c38dfaebdcedbd9012020525430163a7
Thet Min Ko <minko@minko-mac.local>
2020-08-18 13:27:38 +0800

    Update styling

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const ContactCard = () => (
-  <div class="md:flex bg-white rounded-lg p-6 border max-w-md">
-    <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6 object-cover" src={"./avator.jpg"} alt="avatar" />
-    <div class="text-center md:text-left">
-      <h2 class="text-lg">John Doe</h2>
-      <div class="text-purple-500">Software Engineer</div>
-      <div class="text-gray-600">john.doe@ufinity.com</div>
-      <div class="text-gray-600">(+65) 8888 8888</div>
+  <div className="md:flex bg-white rounded-lg p-6 border max-w-md">
+    <img className="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6 object-cover" src={"./avator.jpg"} alt="avatar" />
+    <div className="text-center md:text-left">
+      <h2 className="text-lg font-bold md:font-normal">John Doe</h2>
+      <div className="text-purple-500">Software Engineer</div>
+      <div className="text-gray-600">john.doe@ufinity.com</div>
+      <div className="text-gray-600">(+65) 8888 8888</div>
+      <button className="btn btn-blue mt-4 text-sm">Greet!</button>
     </div>
   </div>
 );
